1528bef096d7ada2688354213395671070ae7b10
akarshjain05 <akarshjain2006@gmail.com>
2026-07-14 12:20:17 +0530

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,7 +62,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
