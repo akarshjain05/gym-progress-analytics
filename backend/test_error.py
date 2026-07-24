e24395d13d2b744de25ab29bff4857b9ef3fa5b2
akarshjain05 <akarshjain2006@gmail.com>
2026-07-24 10:55:06 +0530

    test: configure pytest to only run in tests/ directory and remove test scripts

--- backend/test_error.py
+++
@@ -1,7 +0,0 @@
-import sys
-import math
-from datetime import date, timedelta
-from app.routers.coach import _eta_for_exercise, ist_today
-
-print(_eta_for_exercise(90.0, 100.0, "improving", 2, 0.5, 80.0, 0, 0.9, ist_today(), 20))
-print(_eta_for_exercise(90.0, 100.0, "improving", 1, 0, 0, 0.02, None, ist_today(), 0))
