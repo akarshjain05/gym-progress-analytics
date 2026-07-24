4a861a46d384614bf5f6da7a361d1d1760556fd8
akarshjain05 <akarshjain2006@gmail.com>
2026-07-24 11:13:52 +0530

    chore: remove all one-off patch and migration scripts

--- fix_workout.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const fp = './frontend/js/workout.js';
-let content = fs.readFileSync(fp, 'utf8');
-
-content = content.replace(/loadHistory\(\);/, 'await loadHistory();');
-fs.writeFileSync(fp, content, 'utf8');
