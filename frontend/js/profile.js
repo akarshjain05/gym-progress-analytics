f6e679099f41234ef45f46e17df255a93b44b7b5
akarshjain05 <akarshjain2006@gmail.com>
2026-08-07 23:42:08 +0530

    security: fix XSS vulnerability in escapeHtml

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -122,9 +122,13 @@ document.getElementById("profileForm").addEventListener("submit", async (e) => {
 });
 
 function escapeHtml(str) {
-  const div = document.createElement("div");
-  div.textContent = str;
-  return div.innerHTML;
+  if (str === null || str === undefined) return "";
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
 }
 
 (async function init() {
